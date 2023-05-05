073dbfb49abe0c59921eb07757f5d7994e889e31
Fatih Durmaz <71706970+fatihdurmaz@users.noreply.github.com>
2023-05-05 15:26:07 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Fatih Durmaz on 5.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
